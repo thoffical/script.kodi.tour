5b76fa9d5239319156096adccf12bd123a18e760
Thirdy <163488855+thoffical@users.noreply.github.com>
2025-10-23 13:45:48 +0800

    draft

--- addon.py
+++ addon.py
@@ -5,7 +5,13 @@ addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
 
 # Set a string variable to use 
-line1 = "Hello World! We can write anything we want here Using Python"
+line1 = "Welcome to Kodi!, Select OK to start"
 
 # Launch a dialog box in kodi showing the string variable 'line1' as the contents
 xbmcgui.Dialog().ok(addonname, line1)
+
+# Set a string variable to use 
+line2 = "Kodi is a free open source software licensed under the GPL License"
+
+# Launch a dialog box in kodi showing the string variable 'line1' as the contents
+xbmcgui.Dialog().ok(What is Kodi, line2)
